924ae6b08c10d6f6b3716d7e21ff2aa505092d81
rlopezse <rza.lopez.s@gmail.com>
2026-07-01 12:46:59 -0400

    updated: buttons events y mas

--- app/src/main/java/com/healthpet/MainActivity.kt
+++ app/src/main/java/com/healthpet/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.healthpet
 
-import android.R
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -13,6 +12,8 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.runtime.Composable
 
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.sp
@@ -29,82 +30,84 @@ class MainActivity : ComponentActivity() {
         setContent {
             Column(modifier = Modifier.fillMaxSize()) {
                 Row(modifier = Modifier.fillMaxWidth()) {
-                    Column(
+                    Col(
+                        "Hola",
                         modifier = Modifier
                             .weight(1f)
                             .background(Purple40)
                             .padding(8.dp)
-                    ) {
-                        Text(
-                            text = "Left Col",
-                            fontSize = 14.sp,
-                            color = Color.White
-                        )
-                    }
-                    Column(
+                    )
+                    Col(
+                        "Chao",
                         modifier = Modifier
                             .weight(1f)
-                            .background(Color.Green)
+                            .background(Pink40)
                             .padding(8.dp)
-                    ) {
-                        Text(
-                            text = "Right Col",
-                            fontSize = 14.sp,
-                            color = Color.White
-                        )
-                    }
+                    )
+
                 }
                 Row(modifier = Modifier.fillMaxWidth()) {
-                    Column(
+                    Col(
+                        "Left Col",
                         modifier = Modifier
                             .weight(1f)
                             .background(Pink40)
                             .padding(8.dp, 8.dp)
-                    ) {
-                        Text(
-                            text = "Left Col",
-                            fontSize = 14.sp,
-                            color = Color.White
-                        )
-                    }
-                    Column(
+                    )
+                    Col(
+                        "Center Col",
                         modifier = Modifier
                             .weight(1f)
                             .background(Color.Blue)
                             .padding(8.dp, 8.dp)
-                    ) {
-                        Text(
-                            text = "Center col",
-                            fontSize = 14.sp,
-                            color = Color.White
-                        )
-                    }
-                    Column(
+                    )
+                    Col(
+                        "Right Col",
                         modifier = Modifier
                             .weight(1f)
-                            .background(Color.Red)
+                            .background(Color.Blue)
                             .padding(8.dp, 8.dp)
-                    ) {
-                        Text(
-                            text = "Right Col",
-                            fontSize = 14.sp,
-                            color = Color.White
-                        )
-                    }
-                }
-                Column(Modifier.weight(1f).fillMaxWidth().background(PurpleGrey40)) {
-                    Text(
-                        text = "body",
-                        color = Color.White
                     )
                 }
+                Col(
+                    "Body",
+                    Modifier.weight(1f).fillMaxWidth().background(PurpleGrey40)
+                )
                 Row(modifier = Modifier.fillMaxWidth().background(Purple40).padding(8.dp)) {
                     Text(
                         text = "footer",
                         color = Color.White
                     )
+                    Button(onClick = {
+                        println("prueba desde btn")
+                    }) {
+                        Text("0")
+                    }
+                    Counter()
                 }
             }
+        }
+    }
+
+    @Composable
+    fun Counter()
+    {
+
+    }
+    @Composable
+    fun Col(
+        text: String = "ingresa tu texto",
+        modifier: Modifier
+    ){
+        Column(
+            modifier
+        )
+        {
+            Text(
+                text = text,
+                color = Color.White,
+                fontSize = 14.sp
+            )
         }
     }
 }
