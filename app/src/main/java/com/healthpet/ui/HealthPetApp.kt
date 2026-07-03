e3be6c06518076eab6795e8fed3b62fde1a9fc00
rlopezse <rza.lopez.s@gmail.com>
2026-07-03 10:56:35 -0400

    updated: primer listado y listas

--- app/src/main/java/com/healthpet/ui/HealthPetApp.kt
+++ app/src/main/java/com/healthpet/ui/HealthPetApp.kt
@@ -1,35 +1,38 @@
 package com.healthpet.ui
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
-import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import com.healthpet.models.Pet
-import com.healthpet.models.PetType
-import com.healthpet.theme.Gray_100
 import com.healthpet.theme.White
+import com.healthpet.ui.components.PetCard
 
 @Composable
 fun HealthPetApp(){
-    Column(modifier = Modifier.fillMaxSize().background(Gray_100)) {
-        Column(modifier = Modifier.background(color = White).padding(8.dp)) {
-            val Nori = Pet(1, "\uD83D\uDC31 Nori", PetType.CAT, "Tabby")
-            val Mila = Pet(2, "\uD83D\uDC36 Mila", PetType.DOG, "Pitbul")
-            Text(Nori.name + "\n" +Nori.breed)
-            Text(Mila.name + "\n" +Mila.breed)
-            Button(
-                onClick = {
+    Column(modifier = Modifier.fillMaxSize().background(White).padding(16.dp), verticalArrangement = Arrangement.spacedBy(16.dp)) {
+            PetCard(
+                1,
+                "\uD83D\uDE3A Nori Nori",
+                "Tabby",
+            )
+            PetCard(
+                2,
+                "\uD83D\uDE3A Bru",
+                "Negro",
+            )
 
-                }
+            Button(
+                modifier = Modifier.fillMaxWidth(),
+                onClick = {}
             ) {
                 Text("+ Agregar mascota")
             }
-        }
     }
 }
